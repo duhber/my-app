cefb77ed1d71c61100f6111c93356e0bbc14265e
Lalduhbera HC <duhbera@Lalduhberas-MacBook-Pro.local>
2017-02-19 11:54:01 +0530

    forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   template: `
-    <user></user>
-    <key-up></key-up>
-    <loop-back></loop-back>
     <hero-form></hero-form>
     `,
 })
